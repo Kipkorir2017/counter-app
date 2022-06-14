4b107c27fbcdb45e2d12096bfaffa471544c21fa
Benjamin <pkobenj41@gmail.com>
2022-06-14 09:06:21 +0300

    add count

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 class Counter extends Component {
+  state = {
+    count:1
+  }  
   render() {
     return (
       <React.Fragment>
-        <h1>Hello world</h1>
+        {/* <h1>Hello world</h1> */}
+        <span>{this.state.count}</span>
         <button>increment</button>
       </React.Fragment>
     );
