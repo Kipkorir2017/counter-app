9e2298c8400a71cca846c8f370502b6db09da2ad
Benjamin <pkobenj41@gmail.com>
2022-06-14 12:04:48 +0300

    add changes to counterjs file

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,62 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count:0,
-    imageUrl:'https://picsum.photos/200'
-  } ; 
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+  };
   render() {
     return (
-        <React.Fragment>
-          <img src="{this.state.imageUrl}" alt=""/>
+      <React.Fragment>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  Home <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <img src="{this.state.imageUrl}" alt="" />
         {/* <h1>Hello world</h1> */}
         <span>{this.formatCount()}</span>
         <button>increment</button>
       </React.Fragment>
     );
   }
-  formatCount(){
-    const{count}=this.state;
-    return count === 0 ?<h1>zero</h1>:count;
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>zero</h1> : count;
   }
 }
 
